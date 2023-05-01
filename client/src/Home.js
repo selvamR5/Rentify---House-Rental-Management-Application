115d5b2c4d943c495114d5888c11232e52fb34ae
jaysheri <123jayanth123@gmail.com>
2023-04-30 17:13:00 -0700

    firebase integration

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,13 +1,44 @@
 import RecomApartments from "./components/inc/recom_apartments/RecomApartments";
+import { useState } from "react";
+import { app as hello, storage } from "../src/config/firebaseConfig";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
+import { v4 as uuidv4 } from 'uuid';
 
-function Home(){
-    return (
-      <div className='App'>
-        <div className='app_page'>
-          <RecomApartments/>
-        </div>
+function Home() {
+
+  // State to store uploaded file
+  const [file, setFile] = useState("");
+
+  // Handle file upload event and update state
+  function handleChange(event) {
+    setFile(event.target.files[0]);
+  }
+
+  const handleUpload = async () => {
+    console.log('hegllo')
+    let imgUrl = '';
+    console.log(storage)
+    const storageRef = ref(storage, `houserentalmanagement/images/${uuidv4()}`);
+
+    uploadBytes(storageRef, file).then((snapshot) => {
+      console.log('Uploaded a blob or file!');
+      console.log(snapshot)
+      getDownloadURL(snapshot.ref).then((url) => {
+              console.log(url);
+            });
+    });
+  }
+
+  return (
+    <div className='App'>
+      <div>Hellpo</div>
+      <input type="file" onChange={handleChange} accept="" />
+      <button onClick={handleUpload}>Upload to Firebase</button>
+      <div className='app_page'>
+        <RecomApartments />
       </div>
-    );
+    </div>
+  );
 }
 
 export default Home;
