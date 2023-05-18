a9c911ee18c623245ee88f1d2697b52b72693cee
navya0405 <navya.spin@gmail.com>
2023-05-17 18:20:08 -0700

    added manage property for renter ad tenant

--- client/src/components/inc/sidebar/Sidebar.js
+++ client/src/components/inc/sidebar/Sidebar.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.css';
 import SidebarRow from './SidebarRow';
 
 function Sidebar(props) {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+      setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+      setIsHovered(false);
+    };
+  
     if (!props.showSidebar) {
       return null;
     }
@@ -20,15 +30,22 @@ function Sidebar(props) {
         <Link to="/add-property" onClick={closeSidebar}>
           <SidebarRow title="Add a property" />
         </Link>
-        <Link to="/manage-rental" onClick={closeSidebar}>
+        <Link to="/manage-rental" onClick={closeSidebar} onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}>
           <SidebarRow title="Manage Property" />
+          {isHovered && (
+          <>
+            <SidebarRow title="Manage house - Renter" />
+            <SidebarRow title="Manage house - Tenant" />
+          </>
+        )}
         </Link>
-        <Link onClick={closeSidebar}>
+        {/* <Link onClick={closeSidebar}>
           <SidebarRow title="Tenants portal" />
         </Link>
         <Link onClick={closeSidebar}>
           <SidebarRow title="Landlords portal" />
-        </Link>
+        </Link> */}
       </div>
     );
   }
