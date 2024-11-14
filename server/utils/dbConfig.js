802f1ad417c77315115e8aab21231aae6c024ff1
selvam <selvamrselvammr@gmail.com>
2024-11-14 17:44:38 +0530

    first commit

--- server/utils/dbConfig.js
+++ server/utils/dbConfig.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const config = {
     // mongoDB: process.env.DB_Connection_String
-    mongoDB    : "mongodb+srv://HouseRentalManagementApplication:deejaynavnikSJSU@cluster0.scomigl.mongodb.net/?retryWrites=true&w=majority"
+    mongoDB    : "mongodb+srv://selvamrselvammr:xdHfg8tw5lBXBERy@cluster0.ut5kw.mongodb.net/Rentify?retryWrites=true&w=majority&appName=Cluster0"
 };
 
 module.exports = config;
